caf74e44cb0178055b9361f5c5a9b9f454b8269f
sonu kumar <sonukumar200219@gmail.com>
2025-07-15 00:58:12 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chatapplication-3-d91z.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
@@ -20,7 +20,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chatapplication-3-d91z.onrender.com',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     },
